bef795a3bcdab4704df5eafd81d34687c4f7d70f
kirstendanica <kirdanic@gmail.com>
2023-12-11 15:23:49 -0500

    Setup key structure for CAMOWLFLAGE game

---
+++ main.js
@@ -0,0 +1,9 @@
+// Array for each individual game
+let gameCells = document.querySelectorAll('.game-cell');
+
+// Event listeners to 'click' on each game cell
+gameCells.forEach(cell => {
+    cell.addEventListener('click', function() {
+        // TBC (add game logic)
+    });
+});
